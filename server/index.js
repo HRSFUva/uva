46500a4422f1364413b0ba4e186fa17d2a1de9f5
Preda Anuvatnujotikul <preda.anu@gmail>
2017-03-14 17:29:10 -0700

    Wrote the skeleton of the Express serve

--- server/index.js
+++ server/index.js
@@ -1,37 +1,43 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var db = require(./utilities/dbUtils.js);
+var bodyParser = require('bodyParser');
+var dbUtilities = require('./utilities/dbUtils.js');
 
 //INSTANTIATE APP
 var app = express();
 
-//MIDDLEWARE
+//Middleware for parse application/json
 app.use(bodyParser.json());
 
 //load static files
 // app.use(express.static(__dirname + '../react-client/dist'));
 
-//SET UP ROUTE HANDLING
-//Get request for top wines
+//SETTING UP ALL THE ROUTES FOR THE CLIENT REQUEST
+//GET request for top wines
 app.get('/wine', function(req, res) {
-  console.log('Hello from: inside app.get /wine');
+  console.log('GET request to /wine received');
   res.send('response from app.get /wine');
 });
 
-//Search Requests
+//POST request for search
 app.post('/search', function(req, res) {
-  console.log('hello from inside app.post /search');
+  console.log('POST request to /search received');
   res.send('response from app.post /search');
 });
 
-//Login Requests
+//POST request for signup
+app.post('/signup', function(req, res) {
+  console.log('POST request to /signup received');
+  res.send('response from app.post /signup');
+});
+
+//POST request for login
 app.post('/login', function(req, res) {
-  console.log('hello from inside app.post /search');
+  console.log('POST request to /login received');
   res.send('response from app.post /login');
 });
 
-//Signup Requests
-app.post('/signup', function(req, res) {
-  console.log('hello from inside app.post /signup')
-  res.send('response from app.post /signup');
+//POST request for review
+app.post('/review', function(req, res) {
+  console.log('POST request to /review received');
+  res.send('reponse from app.post /review');
 });
