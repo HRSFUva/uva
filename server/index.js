b0dbf29c7702e9a03b8157a107800c347dafaba0
Hack Reactor Students <accounts@hackreactor.com>
2017-03-15 12:23:49 -0700

    uncomment basic routes

--- server/index.js
+++ server/index.js
@@ -18,34 +18,34 @@ app.get('/', function(req, res) {
   res.send('hello world');
 });
 
-// app.get('/wine', function(req, res) {
-//   console.log('GET request to /wine received');
-//   res.send('response from app.get /wine');
-// });
+app.get('/wine', function(req, res) {
+  console.log('GET request to /wine received');
+  res.send('response from app.get /wine');
+});
 
-// //POST request for search
-// app.post('/search', function(req, res) {
-//   console.log('POST request to /search received');
-//   res.send('response from app.post /search');
-// });
+//POST request for search
+app.post('/search', function(req, res) {
+  console.log('POST request to /search received');
+  res.send('response from app.post /search');
+});
 
-// //POST request for signup
-// app.post('/signup', function(req, res) {
-//   console.log('POST request to /signup received');
-//   res.send('response from app.post /signup');
-// });
+//POST request for signup
+app.post('/signup', function(req, res) {
+  console.log('POST request to /signup received');
+  res.send('response from app.post /signup');
+});
 
-// //POST request for login
-// app.post('/login', function(req, res) {
-//   console.log('POST request to /login received');
-//   res.send('response from app.post /login');
-// });
+//POST request for login
+app.post('/login', function(req, res) {
+  console.log('POST request to /login received');
+  res.send('response from app.post /login');
+});
 
-// //POST request for review
-// app.post('/review', function(req, res) {
-//   console.log('POST request to /review received');
-//   res.send('reponse from app.post /review');
-// });
+//POST request for review
+app.post('/review', function(req, res) {
+  console.log('POST request to /review received');
+  res.send('reponse from app.post /review');
+});
 
 var port = process.env.PORT;
 
