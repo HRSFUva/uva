71aa5c8c54d9f25599369a8b8763a2ec120e68d9
burt23 <mail4mdb@gmail.com>
2017-03-15 22:52:23 -0700

    Added search bar component to app

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -1,29 +1,53 @@
 import React from 'react';
+import Search from './searchBar.jsx';
 
 class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-
+      reviews: [
+      { title: 'speedy',
+        context: 'it was great',
+        rating: 8 },
+      { title: 'speedy',
+        context: 'it was ok',
+        rating: 7 },
+      { title: 'speedy',
+        context: 'it was awesome',
+        rating: 9 },
+      { title: 'speedy',
+        context: 'it was terrible',
+        rating: 6 }
+      ],
+      searchQuery: '',
+      userHasSearched: false
     }
   }
+
+
   render () {
-    return (
-      <div className = 'container'>
-        <div className = 'heroImageContainer'>
-          <span className = 'mainLogo'>
-            <h2>Uva</h2>
-          </span>
-          <div className = 'heroContentWrapper'>
-            <h2>Unbiased wine reviews</h2>
-            <input className = 'mainSearchBar' placeholder = 'Drink Great Wine'></input>
+    if(!this.state.userHasSearched){
+      return (
+        <div className = 'container'>
+          <div className = 'heroImageContainer'>
+            <span className = 'mainLogo'>
+              <h2>Uva</h2>
+            </span>
+            <div className = 'heroContentWrapper'>
+              <h2>Unbiased wine reviews</h2>
+              <Search className ='SearchBar' />
+            </div>
           </div>
         </div>
-
-        hi
-      </div>
-    )
+    )} else {
+        return (
+          <div>Hello after search</div>
+          )
+        //REVIEW LIST
+      }
   }
 }
 
 export default App;
+//<ReviewList reviews={this.state.reviews} />
+
---
+++ react-client/src/components/searchBar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: ''
+    }
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+    this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
+  }
+
+
+  handleSearchSubmit (event) {
+    if(this.state.searchQuery.length){
+      //DO WORK
+    }
+      event.preventDefault();
+  }
+
+  handleSearchChange (event) {
+    this.setState({
+      searchQuery: event.target.value
+    });
+  }
+
+
+render(){
+  return(
+    <form className = 'searchBarForm' onSubmit={this.handleSearchSubmit}>
+      <input className = 'mainSearchBar' onChange={this.handleSearchChange} placeholder = 'Drink Great Wine' value={this.state.searchQuery} />
+      <div className='submitButton'>
+        <input type='submit' value='Submit' />
+      </div>
+    </form>
+    )
+  }
+}
+
+
+export default SearchBar;
