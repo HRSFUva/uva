4c50200da3af1d9bbbf3b69d1102630efe4d66ff
Preda Anuvatnujotikul <preda.anu@gmail>
2017-03-16 15:28:33 -0700

    Reverting back to simple AJAX options.

--- server/utilities/wineApiUtils.js
+++ server/utilities/wineApiUtils.js
@@ -6,15 +6,15 @@ var apiKey = '7e30469636811cfa7dd0aef5dffcddbd';
 var options = {
   method: 'GET',
   url: 'http://services.wine.com/api/beta/service.svc/JSON/catalog?filter=categories(490+124)&offset=10&size=5&apikey=' + apiKey,
-  qs: 
-   { filter: 'categories(490 124)',
-     offset: '10',
-     size: '5',
-     apikey: '7e30469636811cfa7dd0aef5dffcddbd' },
-  headers: 
-   { 'postman-token': 'b781b801-612b-f6aa-3cef-f08dd927b9fc',
-     'cache-control': 'no-cache' },
-  body: 'pa87901'
+  // qs: 
+  //  { filter: 'categories(490 124)',
+  //    offset: '10',
+  //    size: '5',
+  //    apikey: '7e30469636811cfa7dd0aef5dffcddbd' },
+  // headers: 
+  //  { 'postman-token': 'b781b801-612b-f6aa-3cef-f08dd927b9fc',
+  //    'cache-control': 'no-cache' },
+  // body: 'pa87901'
 };
 
 /*
@@ -28,7 +28,7 @@ var apiRequest = function (req, res) {
   request(options, function(error, response, body) {
     if (!error) {
       // console.log('API response', response);
-      console.log('API body', body);
+      console.log('API body', JSON.parse(body));
       res.send(body);
     } else {
       console.error('Error in API request', error);
