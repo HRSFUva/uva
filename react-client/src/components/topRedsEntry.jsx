c8668c07723e610c6c28b40096de980b31ebe713
kevinhuangg <email.kevinh@gmail.com>
2017-03-27 15:57:33 -0700

    make red wines list clickable

--- react-client/src/components/topRedsEntry.jsx
+++ react-client/src/components/topRedsEntry.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-var TopRedsEntry = ({topRed}) => (
-   <div className='productEntryFlexbox'>
+var TopRedsEntry = (props) => (
+   <div className='productEntryFlexbox' onClick={() => { props.handleClickedProductEntry(props.topRed) }}>
     <div className='entryFlexItem' >
-      <h4>{topRed.name}</h4>
-      <p>Released: {topRed.year}</p>
-      <p> Best Price: ${topRed.priceMin}</p>
+      <h4>{props.topRed.name}</h4>
+      <p>Released: {props.topRed.year}</p>
+      <p> Best Price: ${props.topRed.priceMin}</p>
     </div>
     <div className='entryFlexItem flexItemRight'>
       <p>Avg Rating: </p>
-      <h4 className='entryRating'>{topRed.apiRating/20}</h4>
+      <h4 className='entryRating'>{props.topRed.apiRating/20}</h4>
     </div>
    </div>
 
