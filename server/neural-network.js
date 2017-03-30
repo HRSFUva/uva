eacd313fd519880adab9353692b8fc25d6de717b
Benicio Peinado <BenicioPeinado@gmail.com>
2017-03-29 19:24:58 -0700

    Implement NN constructor and trainer functions

--- server/neural-network.js
+++ server/neural-network.js
@@ -4,47 +4,10 @@ module.exports.newUserNN = () => {
   return new synaptic.Architect.Perceptron(6, 3, 1);
 }
 
-const trainingSet = [
-  {
-    input: [0, 0, 0, 0, 0, .2],
-    output: [1]
-  },
-  {
-    input: [0, 0, 0, 0, 0, .9],
-    output: [0]
-  },
-  {
-    input: [0, 0, 0, 0, 0, .4],
-    output: [1]
-  },
-  {
-    input: [0, 0, 0, 0, 0, .1],
-    output: [1]
-  },
-  {
-    input: [0, 0, 0, 0, 0, .8],
-    output: [0]
-  },
-  {
-    input: [0, 0, 0, 0, 0, .6],
-    output: [0]
-  },
-]
-
 module.exports.train = (NN, trainingSet) => {
   const newUserTrainer = new synaptic.Trainer(NN);
   newUserTrainer.train(trainingSet);
   return NN;
 }
-
-let NN = module.exports.newUserNN();
-NN = module.exports.train(NN, trainingSet);
-
-let recs = NN.activate([1, 1, 1, 1, 1, .2]);
-console.log('.2: ', recs);
-recs = NN.activate([1, 0, 1, 0, 1, .4]);
-console.log('.4: ', recs);
-recs = NN.activate([1, 1, 1, 0, 1, .8]);
-console.log('.8: ', recs);
 
 
