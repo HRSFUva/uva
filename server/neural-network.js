d2f150dda9bb63267d4f316a0e8132a6d35900a0
Benicio Peinado <BenicioPeinado@gmail.com>
2017-03-29 17:00:45 -0700

    Pseudocode neural network

---
+++ server/neural-network.js
@@ -0,0 +1,20 @@
+var synaptic = require("synaptic");
+
+// write a function that creates a new NN for every user 
+  // set up input layer, hidden layer, and output layer
+    // input layer has 6 neurons, price, wine type, and 4 varietals
+    // hidden layer has 3 neirons 
+    // one output neuron
+  // set up connections between them 
+    // input is connected to hidden, hidden is connected to output
+
+// function that trains NN - two inputs (NN, trainingData) 
+  // make dummy data to test-train the NN
+  // set up a trainer and pass in data 
+
+// function that activates NN - (NN, inputs) 
+  // return NN output
+
+// test to see how NN performs - pass in various inputs and see what the output is
+  // do a small amount of experimentation to maximize NN's performance 
+    // adjust number of hidden layers and number of neurons 
